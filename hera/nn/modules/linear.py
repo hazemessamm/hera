ba26b94db0850217a19788fb6f99ac60243bd8f6
hazemessamm <hazemessam199@gmail.com>
2023-01-03 18:59:00 +0200

    added linear layer and docstrings

---
+++ hera/nn/modules/linear.py
@@ -0,0 +1,89 @@
+import jax
+from jax import numpy as jnp
+from jax.nn import initializers
+
+from nn.modules import functional as F
+from nn.modules.module import Module
+from nn.modules.parameter import Parameter
+from typing import Dict, Callable, Union
+from jax.numpy import ndarray
+from jax.random import PRNGKey
+
+
+class Linear(Module):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        rng: Union[int, PRNGKey],
+        activation: Callable = None,
+        use_bias: bool = True,
+    ):
+        """Linear Layer which applies linear transformation on the inputs.
+
+        Args:
+            input_dim (int): Size of the input dimension.
+            output_dim (int): Size of the output dimension.
+            rng (int): Seed or a random number that will
+                       be used to create the weights.
+            activation (Callable, optional): An activation function that will
+                                             be called after the linear
+                                             transformation. Defaults to None.
+            use_bias (bool, optional): Whether to use the bias or not.
+                                       Defaults to True.
+        """
+        super().__init__(rng)
+        self.activation = activation
+        self.output_dim = output_dim
+        self.input_dim = input_dim
+        self.use_bias = use_bias
+
+        # Create 2 subkeys for the weights and bias
+        weight_key, bias_key = self.create_keys(2)
+        self.weight = Parameter(
+            weight_key, initializers.glorot_uniform(), (input_dim, output_dim)
+        )
+        if use_bias:
+            self.bias = Parameter(bias_key, initializers.zeros, (output_dim,))
+        self.reset_parameters()
+
+    def compute_output_shape(self, input_shape):
+        inputs = jax.core.ShapedArray((1, *input_shape[1:]), dtype=jnp.float32)
+        shape = jax.eval_shape(self.forward, self.parameters(), inputs).shape
+        return (None, *shape[1:])
+
+    def reset_parameters(self):
+        """Resets (re-intiialize or initialize) Linear module weights.
+        """
+        self.weight.reset_parameter()
+        if self.use_bias:
+            self.bias.reset_parameter()
+
+    def forward(self, weights: Dict, inputs: ndarray) -> ndarray:
+        """Applies linear transformation on the inputs.
+
+        Args:
+            weights (Dict): A dictionary containing the weights
+                            with `weights` and `bias` as keys.
+            inputs (ndarray): A tensor with shape (*, input_dim)
+
+        Returns:
+            ndarray: A linearly transformed input with shape (*, output_dim).
+        """
+        weight = weights["weight"]
+        if self.use_bias:
+            bias = weights["bias"]
+        else:
+            bias = None
+
+        out = F.linear(inputs, weight, bias=bias)
+
+        if self.activation is not None:
+            out = self.activation(out)
+
+        return out
+
+    def __repr__(self):
+        return f"""{self.__class__.__name__}(input_dim={self.input_dim},
+                    output_dim={self.output_dim}, activation={self.activation})
+                """
--- hera/nn/modules/module.py
+++ hera/nn/modules/module.py
@@ -1,9 +1,8 @@
 import abc
 from collections import OrderedDict
-from typing import Any, List, Union
+from typing import Any, List, Tuple, Union
 
 import jax
-
 from nn.modules.parameter import Parameter
 
 
@@ -14,6 +13,31 @@ class Module(abc.ABC):
         stochastic_module: bool = False,
         jit: bool = False,
     ):
+        """Base Module
+
+        Args:
+            rng (Union[int, jax.random.PRNGKey], optional): Seed or default
+                                                            random number for
+                                                            initialization.
+                                                            if set to `None`
+                                                            then this means
+                                                            that this module
+                                                            does not need it.
+                                                            Defaults to None.
+            stochastic_module (bool, optional): If set to `True` then this
+                                                modules will change the seed
+                                                with every call
+                                                (e.g. Dropout needs it to drop
+                                                different neurons every call)
+                                                otherwise it will use the same
+                                                random number.
+                                                Defaults to False.
+            jit (bool, optional): If set to `True` then the `forward()` method
+                                  will be wrapped by `jax.jit()` function.
+                                  if set to `False` it will remain
+                                  a bound method. Defaults to False.
+        """
+
         if rng is not None:
             if isinstance(rng, int):
                 self.rng = jax.random.PRNGKey(rng)
@@ -33,7 +57,6 @@ class Module(abc.ABC):
         self.trainable = True
         self._training = True
         self._name = None
-        self._has_weights = True
 
     @property
     def training(self):
@@ -47,6 +70,7 @@ class Module(abc.ABC):
                 mod.training = val
 
     def reset_parameters(self):
+        """Resets (re-intializes) all of the nested modules."""
         for mod in self.nested_modules:
             if hasattr(mod, "reset_parameters"):
                 mod.reset_parameters()
@@ -54,6 +78,13 @@ class Module(abc.ABC):
                 mod.reset_parameter()
 
     def update_parameters(self, new_weights: OrderedDict):
+        """Updates the weights of the nested modules.
+
+        Args:
+            new_weights (OrderedDict): A dictionary of the same name of the
+                                       attributes holding the nested modules
+                                       as keys and with weights as values.
+        """
         for mod, w in new_weights.items():
             out = getattr(self, mod)
             if isinstance(out, Parameter):
@@ -62,10 +93,22 @@ class Module(abc.ABC):
                 out.update_parameters(w)
 
     def parameters(self):
+        """Returns the nested modules parameters.
+
+        Returns:
+            Dict: A dictionary of the same name of the attributes holding the
+                  nested modules as keys and with weights as values
+        """
         return self.state_dict()
 
-    def load_state_dict(self, new_weights: OrderedDict):
-        for k, v in new_weights.items():
+    def load_state_dict(self, state: OrderedDict):
+        """Loads the state of the module and its nested modules.
+
+        Args:
+            state (OrderedDict): State with the attribute names as keys
+                                 and tensors as values.
+        """
+        for k, v in state.items():
             out = getattr(self, k)
             if isinstance(out, Parameter):
                 out.data = v
@@ -73,6 +116,11 @@ class Module(abc.ABC):
                 out.load_state_dict(v)
 
     def state_dict(self):
+        """Returns the current state of the module and its nested modules.
+
+        Returns:
+            Dict: Attribute names as keys and tensors as values.
+        """
         out = OrderedDict()
         for mod in self.nested_modules:
             if isinstance(mod, Module):
@@ -84,26 +132,58 @@ class Module(abc.ABC):
         return out
 
     def eval(self):
+        """Changes the behaviour of the module for evaluation.
+        """
         if self._training:
             self.training = False
 
     def train(self):
+        """Changes the behaviour of the module for training.
+        """
         if not self._training:
             self.training = True
 
     def reset_rng(self):
+        """Resets the current seed to the initial seed.
+
+        Raises:
+            ValueError: If the module is not stochastic.
+        """
         if self.stochastic_module:
             self.rng = self._initial_rng
         else:
             raise ValueError(f"{self} is not a stochastic module.")
 
-    def compute_output_shape(self, input_shape):
+    def compute_output_shape(self, input_shape: Tuple):
+        """Utiliity function to calculate the output of a module without
+           calling the forward function.
+
+        Args:
+            input_shape (Tuple): A tuple with example input shape
+                                 (Add a dummy batch_size in the first index.).
+
+        Returns:
+            Tuple: Output shape.
+        """
         return input_shape
 
     def create_keys(self, n):
+        """Creates N keys from the initial seed.
+
+        Args:
+            n (int): Number of keys to be created.
+
+        Returns:
+            ndarray: A tensor with N keys.
+        """
         return jax.random.split(self.rng, n)
 
     def make_random_key(self):
+        """Returns a new random key each time it gets called.
+
+        Returns:
+            ndarray: A new key.
+        """
         if self.stochastic_module:
             rng, another_key = jax.random.split(self.rng, 2)
             if not isinstance(self.rng, jax.core.Tracer):
@@ -115,9 +195,10 @@ class Module(abc.ABC):
     def __setattr__(self, __name: str, __value: Any) -> None:
         if isinstance(__value, (Module, Parameter)):
             if not hasattr(self, "nested_modules"):
-                raise Exception(
-                    "super().__init__() is not called. Please add it in your subclass `__init__`"
-                )
+                msg = """super().__init__() is not called.
+                         Please add it in your subclass `__init__`
+                      """
+                raise Exception(msg)
             self.nested_modules.append(__value)
             __value._name = __name
 
@@ -152,6 +233,11 @@ class Module(abc.ABC):
         return self.forward(weights, *args, **kwargs)
 
     def __call__(self, *args, **kwargs):
+        """Calls the pre_forward (if implemented) and the forward function.
+
+        Returns:
+            ndarray: An output tensor.
+        """
         self.jit_forward()
 
         if self._has_weights:
@@ -159,8 +245,5 @@ class Module(abc.ABC):
             out = self._forward_with_weights(weights, *args, **kwargs)
         else:
             out = self.forward(*args, **kwargs)
-        
+
         return out
-
-
-        

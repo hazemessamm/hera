959406d8d24a91374ad7a007b853f4c37f7c5228
hazemessamm <hazemessam199@gmail.com>
2023-01-09 15:42:38 +0200

    added docstrings for BackwardRecorder class

--- hera/nn/training_utils.py
+++ hera/nn/training_utils.py
@@ -1,6 +1,6 @@
 from contextlib import contextmanager
 from functools import partial
-from typing import Tuple, Union
+from typing import Dict, Tuple, Union
 
 import jax
 import optax
@@ -24,13 +24,38 @@ def eval_mode(model):
 
 class BackwardRecorder:
     def __init__(self, module: Module, loss: Loss, optimizer: Optimizer = None):
+        """Applies forward and backward propagation.
+
+        Args:
+            module (Module): The module that will make the inference and
+                             retrieve its gradients.
+            loss (Loss): The loss function that will be used for calculating
+                         the gradients.
+            optimizer (Optimizer, optional): If the optimizer is passed then
+                                             `.apply_updates()` method could be
+                                             used to call the optimizer and
+                                             update the model weights.
+                                             Defaults to None.
+        """
         self.module = module
         self.loss = loss
         self.optimizer = optimizer
 
     def backward(
-        self, weights, *args, targets: Union[Tuple, ndarray], **kwargs
-    ):
+        self, weights: Dict, *args, targets: Union[Tuple, ndarray], **kwargs
+    ) -> Tuple[ndarray, ndarray, Dict]:
+        """Applies backward propagation.
+
+        Args:
+            weights (Dict): Dictionary of attribute names as keys and
+                            weights as values.
+            targets (Union[Tuple, ndarray]): The labels that will be used in
+                                             the loss function.
+
+        Returns:
+            Tuple[ndarray, ndarray, Dict]: Returns the loss value, predictions
+                                           and the gradients as a dictionary.
+        """
         @partial(jax.value_and_grad, argnums=0, has_aux=True)
         def _backward(weights, *args, targets, **kwargs):
             preds = self.module(weights, *args, **kwargs)
@@ -39,7 +64,20 @@ class BackwardRecorder:
 
         return _backward(weights, *args, targets=targets, **kwargs)
 
-    def apply_updates(self, gradients, params):
+    def apply_updates(self, gradients: Dict, params: Dict):
+        """Update the weights using the assigned optimizer and
+           update the model with the new weights.
+
+        Args:
+            gradients (Dict): Dictionary of attribute names as keys and
+                              gradients as values. 
+            params (Dict): Dictionary of attribute names as keys and
+                           weights as values.
+
+        Raises:
+            ValueError: `.apply_updates()` cannot update the weights while not
+                        passing the optimizer in `__init__()`
+        """
         if self.optimizer is not None:
             new_weights = self.optimizer.update_weights(
                 gradients=gradients, params=params
@@ -48,7 +86,8 @@ class BackwardRecorder:
         else:
             raise ValueError(
                 "Expected optimizer with type `Optimizer`. "
-                f"Recieved {self.optimizer}"
+                f"Recieved {self.optimizer}. Pass the optimizer in the __init__"
+                " (e.g. __init__(model=model, loss=loss, optimizer=optimizer))"
             )
 
     def __enter__(self, *args, **kwargs):

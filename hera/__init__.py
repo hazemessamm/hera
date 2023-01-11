281fbe7d5960175d4c40b86a085233d356469847
hazemessamm <hazemessam199@gmail.com>
2023-01-11 22:06:40 +0200

    updated __init__.py

--- hera/__init__.py
+++ hera/__init__.py
@@ -4,3 +4,7 @@ from .nn.training_utils import BackwardRecorder
 from .nn import optimizers
 from .backend import auto_register_enabled
 from .backend import enable_auto_register
+from .backend import platform
+from .backend import devices
+from .backend import is_gpu_available
+from .backend import device_count
